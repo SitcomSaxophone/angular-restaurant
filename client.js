1d69a3504e87e3639c4c938d64d3929af675dbfc
Samuel Solberg <pauliesays65@gmail.com>
2018-09-12 14:14:10 -0500

    list appending to DOM

--- client.js
+++ client.js
@@ -7,9 +7,6 @@ foodApp.controller('FoodController', function () {
     let self = this;
 
     self.message = 'Hello';
-    self.foods = [
-        {
-
-        }
-    ]
+    self.foods = ['steak', 'lobster', 'coq a vin', 'spam with plain white rice'];
+    
 })
